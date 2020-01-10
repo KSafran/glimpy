028736d947d7a400b651b1a367bb2edfc892f041
ksafran <ksafran356@gmail.com>
2020-01-09 22:01:25 -0500

    more cleanup

--- glimpy/__init__.py
+++ glimpy/__init__.py
@@ -5,8 +5,6 @@ aims to follow the scikit-learn API closely enough to improve ease of
 use and to take advantage of useful scikit-learn tools such as 
 `sklearn.pipeline.Pipeline` and `sklearn.model_selection.cross_val_score`.
 '''
-# from .poisson import PoissonGLM
-# from .gamma import GammaGLM
-# from .normal import NormalGLM
+from .glm import GLM
 
-# __all__ = ["PoissonGLM", "GammaGLM", "NormalGLM"]
+__all__ = ["GLM"]
