6a5897a537873a1a8854733c536aaf0e074dddea
ksafran <ksafran356@gmail.com>
2020-01-13 15:48:09 -0500

    add more * imports

--- glimpy/__init__.py
+++ glimpy/__init__.py
@@ -1,19 +1,20 @@
 '''Generalized Linear Models in Python
 
-glimpy is a Python module for fitting generalized linear models. It 
-aims to follow the scikit-learn API closely enough to improve ease of 
-use and to take advantage of useful scikit-learn tools such as 
+glimpy is a Python module for fitting generalized linear models. It
+aims to follow the scikit-learn API closely enough to improve ease of
+use and to take advantage of useful scikit-learn tools such as
 `sklearn.pipeline.Pipeline` and `sklearn.model_selection.cross_val_score`.
 '''
 from .glm import GLM
 from .families import (
-    Gaussian, 
+    Gaussian,
     InverseGaussian,
     NegativeBinomial,
     Tweedie,
-    Binomial, 
-    Gamma, 
+    Binomial,
+    Gamma,
     Poisson
     )
 
-__all__ = ["GLM"]
+__all__ = ["GLM", "Gaussian", "InverseGaussian", "NegativeBinomial", "Tweedie",
+    "Binomial", "Gamma", "Poisson"]
